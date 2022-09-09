798cbef4d74c3a574155818f1255926864b3ceb8
Zurishaddai Temitope <temitopezurishaddai@gmail.com>
2022-09-09 12:32:57 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const togglebtn = document.getElementsByClassName('toggle-btn')[0]
-const navbar = document.getElementsByClassName('links')[0]
+const togglebtn = document.getElementsByClassName('toggle-btn')
+const navbar = document.getElementsByClassName('links')
 
 togglebtn.addEventListener('click', () => {
 
     navbar.classList.toggle('active')
 })
+
+
